6aa84c33004b47c00b4c3aa77cb180433e2fdf07
William Pscheidt <williampscheidt@hotmail.com>
2023-03-10 13:38:38 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -22,12 +22,64 @@ function App() {
           <SidebarMenu />
         </SidebarContainer>
         <ContentContainer>
-          <div style={{"width": 500, "height": 300, "display": "flex"}}>
-            <TotalLigacoes />
-            <TotalLigacoes />
-            <Avaliacoes />
+          <div className="dashboard-grid">
+            <div class="chart chart-1">
+              <div class="chart_internal">
+                <div class="chart_informations">
+                  <span className="sub-text">Total de Ligações</span>
+                  <span className="main-text">5.987</span>
+                </div>
+                <div class="chart_graphy">
+                  <TotalLigacoes />
+                </div>
+              </div>
+            </div>
+            <div class="chart chart-2">
+              <div class="chart_internal">
+                <div class="chart_informations">
+                  <span className="sub-text">Ligações mais Longas</span>
+                  <span className="main-text">05 de Março às 19:00</span>
+                </div>
+                <div class="chart_graphy">
+                  <TotalLigacoes />
+                </div>
+              </div>
+            </div>
+            <div class="chart chart-3">
+              <div class="chart_internal">
+                <div class="chart_informations">
+                  <span className="sub-text">Tempo Médio</span>
+                  <span className="main-text">Média</span>
+                </div>
+                <div class="chart_graphy">
+                  <TotalLigacoes />
+                </div>
+              </div>
+            </div>
+            <div class="chart chart-4">
+              <div class="chart_internal">
+                <div class="chart_informations">
+                  <span className="sub-text">Média de Avaliações</span>
+                  <span className="main-text">Ótimo</span>
+                </div>
+                <div class="chart_graphy">
+                  <Avaliacoes />
+                </div>
+              </div>
+            </div>
+            <div class="chart chart-5">
+              <div class="chart_internal">
+                <div class="chart_informations">
+                  <span className="sub-text">Pausas</span>
+                  <span className="main-text">Almoço & Improdutiva</span>
+                </div>
+                <div class="chart_graphy">
+                  <TotalLigacoes />
+                </div>
+              </div>
+            </div>
+            {/* <TotalLigacoes /> <Avaliacoes />*/}
           </div>
-          
         </ContentContainer>
       </div>
     </div>
